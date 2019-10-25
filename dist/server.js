dccd0fbc375e4cd9583b89724fc7183c0d69fe88
zhenkai <zhenkai.chen.work@gmail.com>
2019-10-25 16:31:59 +0200

    deployment v1.2

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const serveStatic = require('serve-static')
 
 const app = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8081
 
 app.use(serveStatic(__dirname))
 app.listen(port)
