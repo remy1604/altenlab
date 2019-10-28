f21970b134cf249e2038385e3f5e39e5710a73ff
zhenkai <zhenkai.chen.work@gmail.com>
2019-10-28 17:56:03 +0100

    deployment v2.0

--- deploy.sh
+++ deploy.sh
@@ -9,15 +9,9 @@ npm run build
 # cd 到构建输出的目录下
 cd dist
 
-# 部署到自定义域域名
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
-
-# 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:remy1604/altenlab.git master:gh-pages
